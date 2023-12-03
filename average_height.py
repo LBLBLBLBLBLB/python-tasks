494afe03ab1dadd7b265bc2779cdeb2b2f86b270
Levani Birchadze <levanbirchadze@gmail.com>
2023-12-03 16:31:13 +0400

    python tasks

---
+++ average_height.py
@@ -0,0 +1,13 @@
+students_height = input("input a list of student heights").split()
+
+for n in range(0, len(students_height)):
+    students_height[n] = int(students_height[n])
+print(students_height)
+
+total_height = 0
+for i in range(0, len(students_height)):
+    total_height += students_height[i]
+
+average_height = round(total_height/len(students_height))
+print(average_height)
+
