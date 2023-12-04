9525bc2df73b7f0bb34dbdd77b263a5e14ebe550
Levani Birchadze <levanbirchadze@gmail.com>
2023-12-04 18:30:19 +0400

    add more tasks

---
+++ caesar_cipher.py
@@ -0,0 +1,14 @@
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+#            'w', 'x', 'y', 'z']
+#
+# direction = input("type 'encode' to encrypt, 'decode' to decrypt: \n")
+# text = input("type your message: \n").lower()
+# shift = int(input("type the shift number: \n"))
+#
+#
+# def encrypt(text, shift):
+#     for i in range(0, len(text)):
+#         text[i] = letters[i + shift]
+#
+#
+# print(encrypt(text, shift))
