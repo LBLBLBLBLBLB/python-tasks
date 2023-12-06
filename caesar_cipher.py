30112db11d01af3f4694005deea0a691c38705bd
Levani Birchadze <levanbirchadze@gmail.com>
2023-12-06 23:47:28 +0400

    add task

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,14 +1,25 @@
-# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-#            'w', 'x', 'y', 'z']
-#
-# direction = input("type 'encode' to encrypt, 'decode' to decrypt: \n")
-# text = input("type your message: \n").lower()
-# shift = int(input("type the shift number: \n"))
-#
-#
-# def encrypt(text, shift):
-#     for i in range(0, len(text)):
-#         text[i] = letters[i + shift]
-#
-#
-# print(encrypt(text, shift))
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+           's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("type 'encode' to encrypt, 'decode' to decrypt: \n")
+text = input("type your message: \n").lower()
+shift = int(input("type the shift number: \n"))
+
+
+def caesar(plain_text, shift_amount, cipher_direction):
+    final_text = ""
+    for i in plain_text:
+        position = letters.index(i)
+
+        if cipher_direction == 'encode':
+            new_position = position + shift_amount
+        else:
+            new_position = position - shift_amount
+
+        new_letter = letters[new_position]
+        final_text += new_letter
+    print(f"the {cipher_direction}d text is {final_text}")
+
+
+caesar(text,shift,direction)
